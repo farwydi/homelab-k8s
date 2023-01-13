7c135c26522d767dc8dc7b696e515d9da69e5f46
leonid zharikov <leonid.zharikov@quadcode.com>
2023-01-13 22:46:00 +0200

    refactor: move project to self folders

---
+++ system/lgtm/secrets/create.sh
@@ -0,0 +1,2 @@
+kubeseal --format yaml > ../templates/grafana-cloud-logs.yaml < grafana-cloud-logs.yaml
+kubeseal --format yaml > ../templates/grafana-cloud-metrics.yaml < grafana-cloud-metrics.yaml
---
+++ system/traefik/secrets/create.sh
@@ -0,0 +1,10 @@
+kubectl create secret tls cloudflare-client-tls \
+  --cert=cloudflare-client-cert.pem \
+  --key=cloudflare-client-key.pem \
+  -n traefik \
+  --dry-run=client -o=yaml | kubeseal --format yaml > ../templates/cloudflare-client-tls.yaml
+
+kubectl create secret generic cloudflare-origin-ca-ecc-root-cert \
+  --from-file=cloudflare-origin-ca-ecc-root.crt \
+  -n traefik \
+  --dry-run=client -o=yaml > ../templates/cloudflare-origin-ca-ecc-root-cert.yaml
