d716c573919d479907f5d8a6523b43eb5b9e2bbd
leonid zharikov <leonid.zharikov@quadcode.com>
2023-01-18 18:09:21 +0200

    feat(traefik): improve secure SSL

--- system/traefik/secrets/create.sh
+++ system/traefik/secrets/create.sh
@@ -8,3 +8,8 @@ kubectl create secret generic cloudflare-origin-ca-ecc-root-cert \
   --from-file=cloudflare-origin-ca-ecc-root.crt \
   -n traefik \
   --dry-run=client -o=yaml > ../templates/cloudflare-origin-ca-ecc-root-cert.yaml
+
+kubectl create secret generic cloudflare-origin-pull-ca-cert \
+  --from-file=ca.crt \
+  -n traefik \
+  --dry-run=client -o=yaml | kubeseal --format yaml > ../templates/cloudflare-origin-pull-ca-cert.yaml
